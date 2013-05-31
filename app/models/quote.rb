445f51ba89dd1d6bc5c9cf6f182a8bf7361f9a2e
Lennon Chimbumu <lennonc@stanford.edu>
2013-05-31 12:47:24 -0700

    Adds rspec

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,6 @@
 class Quote < ActiveRecord::Base
   attr_accessible :date_of_quote, :quote, :speaker, :user_id
+
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password_digest, :password_salt
+
+  # creates one to many association with quotes so we can call user.quotes 
+  # on a user object (add a belongs to association to the quotes)
+  has_many :quotes
+
 end
